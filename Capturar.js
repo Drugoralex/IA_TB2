81c207977e80045387c8973cbf84825c2e7f13e6
LuisGH1234 <u201516808@upc.edu.pe>
2018-11-16 23:02:51 -0500

    refactor

--- Capturar.js
+++ Capturar.js
@@ -1,27 +1,18 @@
 $(document).ready(()=>{
     $('#Enviar').click(async ()=>{
-    	var lis = document.getElementById("div2").getElementsByTagName("li");
+    	var lis = [...document.getElementById("div2").getElementsByTagName("li")];
    		var Dinero = document.getElementById("Dinero");
    		var Procesador = document.getElementById("Procesador");
    		var Tarjeta = document.getElementById("Tarjeta");
    		var Ram = document.getElementById("Ram");
 
-    	/*for (i = 0;	i < lis.length ; i++) {
-    		console.log(lis[i].value);
-		}*/
     	var auxDinero = Dinero.options[Dinero.selectedIndex].value;
     	var auxProcesador = Procesador.options[Procesador.selectedIndex].value;
     	var auxTarjeta = Tarjeta.options[Tarjeta.selectedIndex].value;
     	var auxRam = Ram.options[Ram.selectedIndex].value;
 		
 		let data = {
-			lista_juegos: function() {
-				let array = [];
-				for(let i = 0;i<lis.length;i++){
-					array.push(lis[i].value);
-				}
-				return array;
-			}(),
+			lista_juegos: lis.map((ele, i) => ele.value),
 			dinero: auxDinero
 		};
 
